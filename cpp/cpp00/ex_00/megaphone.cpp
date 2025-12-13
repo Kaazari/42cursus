7fc1fa190c68ed1d5e802706e289516f088c55dc
Zakaria Djitte <zdjitte@f1r2s9.clusters.42paris.fr>
2025-12-13 19:35:23 +0100

    megaphone finished, started phonebook

--- cpp/cpp00/ex_00/megaphone.cpp
+++ cpp/cpp00/ex_00/megaphone.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
 #include <cctype>
+#include <string>
 
 int	main(int ac, char **av) {
 	if (ac != 1)
 	{
-		int i = 0;
-		while (av[1][i])
-			std::cout << toupper(av[1][i++]) << std::endl;
+		int i = 1;
+		while (av[i])
+		{
+			int j = 0;
+			std::string str = av[i];
+			while (j < str.length())
+				std::cout << (char)toupper(str[j++]);
+			i++;
+		}
+		std::cout << std::endl;
+		return (0);
 	}
+	std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	return (1);
 }
---
+++ cpp/cpp00/ex_01/PhoneBook.hpp
@@ -0,0 +1,14 @@
+#ifndef PHONE_BOOK_HPP
+# define PHONE_BOOK_HPP
+
+class Contact {
+	public:
+
+};
+
+class PhoneBook {
+	private:
+		
+};
+
+#endif
