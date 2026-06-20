09a0cdd63bb46c8372a72d218f0e0a738cc90089
Kaazari <kaazarikuso@gmail.com>
2026-06-20 03:01:19 +0200

    fix ex00 cpp06

--- cpp/cpp06/ex00/ScalarConverter.cpp
+++ cpp/cpp06/ex00/ScalarConverter.cpp
@@ -12,6 +12,14 @@ ScalarConverter& ScalarConverter::operator=(const ScalarConverter& other) {
 ScalarConverter::~ScalarConverter() {}
 
 void ScalarConverter::Convert(const std::string& input) {
+	if (input.length() == 3 && input[0] == '\'' && input[2] == '\'') {
+		char c = input[1];
+		std::cout << "char: '" << c << "'" << std::endl;
+		std::cout << "int: " << static_cast<int>(c) << std::endl;
+		std::cout << "float: " << std::fixed << std::setprecision(1) << static_cast<float>(c) << "f" << std::endl;
+		std::cout << "double: " << std::fixed << std::setprecision(1) << static_cast<double>(c) << std::endl;
+		return;
+	}
 	if (input == "nan" || input == "nanf") {
 		std::cout << "char: impossible\nint: impossible\nfloat: nanf\ndouble: nan" << std::endl;
 	}
@@ -41,7 +49,10 @@ void ScalarConverter::PrintChar(const std::string& input) {
 	}
 	int n;
 	std::istringstream iss(input);
-	iss >> n;
+	if (!(iss >> n) || !iss.eof()) {
+		std::cout << "char: impossible" << std::endl;
+		return;
+	}
 	unsigned char c = static_cast<unsigned char>(n);
 	if (c >= 32 && c <= 127) {
 		std::cout << "char: " << "'" << c << "'" << std::endl;
@@ -61,6 +72,10 @@ void ScalarConverter::PrintInt(const std::string& input) {
 	else {
 		float i;
 		std::istringstream iss(input);
+		if (!(iss >> i) || !iss.eof()) {
+		std::cout << "int: impossible" << std::endl;
+		return;
+	}
 		if (iss >> i)
 			std::cout << "int: " << static_cast<int>(i) << std::endl;
 		else
@@ -76,6 +91,10 @@ void ScalarConverter::PrintFloat(const std::string& input) {
 	}
 	float f;
 	std::istringstream iss(input);
+	if (!(iss >> f) || !iss.eof()) {
+		std::cout << "float: impossible" << std::endl;
+		return;
+	}
 	if (iss >> f) {
 		std::cout << "float: " << std::fixed << std::setprecision(1) << f << "f" << std::endl;
 	}
@@ -91,6 +110,10 @@ void ScalarConverter::PrintDouble(const std::string& input) {
 	else {
 		float i;
 		std::istringstream iss(input);
+		if (!(iss >> i) || !iss.eof()) {
+			std::cout << "double: impossible" << std::endl;
+			return;
+		}
 		if (iss >> i)
 			std::cout << "double: " << std::fixed << std::setprecision(1) << static_cast<double>(i) << std::endl;
 		else
